7ade7c38a808167d642ea5260708831e1fd08ce3
jefferson <jeffersonluisdesousa1@gmail.com>
2022-10-19 06:56:38 -0300

    Primeiro commit flask

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
+class jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
 
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    jogo1 = jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = jogo('God Of War', 'Rack n Slash', ' PS2')
+    jogo3 = jogo('Mortal Combate', 'Luta', 'PS2')
+
+    lista = [jogo1, jogo2, jogo3]
+
+    return render_template('lista.html', titulo = 'Jogos', jogos = lista )
 
 app.run()
--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/inicio')
-def ola():
-    return '<h1>Olá Mundo!</h1>'
-
-app.run()
